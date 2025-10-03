32b2d869d92a85e57f9a40aea992d15b123d7bb9
IvanSimcic08 <ivan.simcic3@gmail.com>
2025-10-03 15:26:25 +0200

    Zad13.js

--- JavascriptIvanŠ/Zad13.js
+++ JavascriptIvanŠ/Zad13.js
@@ -1,9 +1,1 @@
-const prompt = require('prompt-sync')();
-let ocjene = [];
 
-for (let i = 0; i < 3; i++) {
-    ocjene.push(parseFloat(prompt(`Unesi ocjenu ${i + 1}: `)));
-}
-
-let prosjek = (ocjene[0] + ocjene[1] + ocjene[2]) / 3;
-console.log("Prosjek je:", prosjek.toFixed(2));
