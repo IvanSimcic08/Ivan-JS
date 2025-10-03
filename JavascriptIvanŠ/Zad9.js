af1b326a2db9046c496ff6d72eac4ad74c7be0cd
IvanSimcic08 <ivan.simcic3@gmail.com>
2025-10-03 15:27:12 +0200

    Zad9.js

--- JavascriptIvanŠ/Zad9.js
+++ JavascriptIvanŠ/Zad9.js
@@ -1,12 +1,1 @@
-function brojSamoglasnika(str) {
-    let samoglasnici = "aeiouAEIOU";
-    let broj = 0;
-    for (let slovo of str) {
-        if (samoglasnici.includes(slovo)) {
-            broj++;
-        }
-    }
-    return broj;
-}
 
-console.log(brojSamoglasnika("Programiranje"));
