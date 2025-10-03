0bc4550792e94be361ae3054f530d7820127bb16
IvanSimcic08 <ivan.simcic3@gmail.com>
2025-10-03 15:23:17 +0200

    Zad19.js

--- JavascriptIvanŠ/Zad19.js
+++ JavascriptIvanŠ/Zad19.js
@@ -1,8 +1,3 @@
-const prompt = require('prompt-sync')();
-let godine = parseInt(prompt("Unesi svoje godine: "));
 
-if (godine >= 18) {
-    console.log("Možeš glasati");
-} else {
-    console.log("Ne možeš glasati");
-}
+
+
